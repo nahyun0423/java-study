81bbad53d0d7a902e3813ab2727cc59ad7be628a
nahyun0423 <68987116+nahyun0423@users.noreply.github.com>
2023-04-06 16:20:44 +0900

    Add files via upload

---
+++ src/Q2753.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Q2753 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int year = scan.nextInt();
+
+        if (year % 4 == 0) {
+            if (year % 100 != 0) {
+                System.out.println(1);
+            } else if (year % 400 == 0) {
+                System.out.println(1);
+            } else {
+                System.out.println(0);
+            }
+        } else {
+            System.out.println(0);
+        }
+    }
+}
